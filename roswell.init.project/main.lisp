43881d0dbac3526e1f308090dac6f182a0f45683
SANO Masatoshi <snmsts@gmail.com>
2018-01-28 02:51:52 +0900

    make it able to specify directory.

--- roswell.init.project/main.lisp
+++ roswell.init.project/main.lisp
@@ -7,7 +7,7 @@
                              :defaults *load-pathname*
                              :name n
                              :type "asd"))))
-(defun prepare-project (names dir)
+(defun prepare-project (name dir)
   (let ((path (merge-pathnames (make-pathname :name "project" :type "lisp") dir)))
     (unless (with-open-file (out path
                                  :direction :output
@@ -16,7 +16,7 @@
               (when out
                 (let ((*package* (find-package :roswell.init.project)))
                   (format out ";;don't edit~%~(~S~)~%"
-                          `(let ((data '(("asd" . ,names))))
+                          `(let ((data '(("asd" . ,(list name)))))
                              ,*loader*)))
                 (format t "~&Successfully generated: ~A~%" path)
                 t))
@@ -24,37 +24,32 @@
       (roswell:quit 1))
     t))
 
-(defun prepare-asd (names dir)
-  (loop for name in names
-     for path = (merge-pathnames (make-pathname :name name :type "asd") dir)
-     always (or (with-open-file (out path
-                                     :direction :output
-                                     :if-exists nil
-                                     :if-does-not-exist :create)
-                  (when out
-                    (format out ";;don't edit~%(defsystem ~S)" name)
-                    (format t "~&Successfully generated: ~A~%" path)
-                    t))
-                (format *error-output* "~&File already exists?: ~A~%" path)
-                (probe-file path)
-                (format *error-output* "~&Fail: ~A~%" path))))
+(defun prepare-asd (name dir)
+  (let ((path (merge-pathnames (make-pathname :name name :type "asd") dir)))
+    (or (with-open-file (out path
+                             :direction :output
+                             :if-exists nil
+                             :if-does-not-exist :create)
+          (when out
+            (format out ";;don't edit~%(defsystem ~S)" name)
+            (format t "~&Successfully generated: ~A~%" path)
+            t))
+        (format *error-output* "~&File already exists?: ~A~%" path)
+        (probe-file path)
+        (format *error-output* "~&Fail: ~A~%" path))))
 
-(defun project (project &rest names)
-  (setf names (list (or (first names) "default"))
-        names (mapcar (lambda (name)
-                        (setf name (string name))
-                        (map () (lambda (i)
-                                  (setf name (remove i name)))
-                             "/\\")
-                        name)
-                      names))
-  (let ((dir (format nil "~A/" (first names))))
+(defun project (project &rest params)
+  (let* ((name (let ((i (or (first params) "default")))
+                 (loop for a across "/\\"
+                    do (setf i (remove a i)))
+                 i))
+         (dir (format nil "~A/" (or (second params) name))))
     (when (probe-file dir)
-      (format *error-output* "~&Directory already exists: ~A~%" (first names))
+      (format *error-output* "~&Directory already exists: ~A~%" dir)
       (roswell:quit 1))
     (ensure-directories-exist dir)
-    (unless (and (ignore-errors (prepare-project names dir))
-                 (prepare-asd names dir))
+    (unless (and (ignore-errors (prepare-project name dir))
+                 (prepare-asd name dir))
       (let ((path (make-pathname :defaults "project" :type "lisp")))
         (format *error-output* "~&Delete: ~A~%" path)
         (delete-file path)))))
